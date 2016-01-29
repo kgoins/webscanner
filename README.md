d8f9517938ead6d1eebb11b39a89ff35124e313d
kgoins <kgoins@github.com>
2016-01-29 17:33:34 -0500

    no longer requires http://www. in url input

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # webscanner
 A python based web scanner that delivers in-depth information about a target URL for use in penetration testing
-
-Known issues:
-you must input a valid url (ie. http://www.google.com) vs google.com
--- scanner.py
+++ scanner.py
@@ -15,34 +15,34 @@ def driver():
         print "Example: python driver.py http://www.google.com outfile.txt"
         sys.exit()
 
-    url_tld = ""
+
     if "http://www." not in url:
-    	url_tld = url
-    	url = "http://www." + url
+        url_tld = url
+        url = "http://www." + url
     else:
-    	url_tld = get_tld(url)
+        url_tld = get_tld(url)
 
-	# run scans
-	url_ip = get_ip(url_tld)
-	robots = get_robots(url)
-	whois = get_whois(url_tld)
+    # run scans
+    url_ip = get_ip(url_tld)
+    robots = get_robots(url)
+    whois = get_whois(url_tld)
 
-	# output data
-	data = "Url IPs:\n" + str(url_ip) + "\n" + \
-		"Url's whois file:\n" + str(whois) + \
-		"Url's robots.txt file:\n" + str(robots)
-	
-	if os.path.exists(file_path):
-		os.remove(file_path)
-	
-	write_file(data, file_path)
+    # output data
+    data = "Url IPs:\n" + str(url_ip) + "\n" + \
+        "Url's whois file:\n" + str(whois) + \
+        "Url's robots.txt file:\n" + str(robots)
+
+    if os.path.exists(file_path):
+        os.remove(file_path)
+
+    write_file(data, file_path)
 
 
 def write_file(data, file_path):
-	f = open(file_path, 'a')
-	f.write(data)
-	f.close()
+    f = open(file_path, 'a')
+    f.write(data)
+    f.close()
 
 
 if __name__ == "__main__":
-	driver()
+    driver()
